9ea1b806fb91b6e50450354ebcc1e1d4c459d853
Stefan Benz <stbenz88@gmail.com>
2017-07-15 21:03:48 +0200

    add async await example

---
+++ async-await-03.js
@@ -0,0 +1,11 @@
+const request = require('request-promise');
+
+(async () => {
+    const users = await request.get({ url: 'http://localhost:3000/users', json: true });
+    const [firstUser] = users;
+
+    const age = await request.get({ url: 'http://localhost:3000/users/' + firstUser + '/age', json: true });
+    const {userAge} = age;
+
+    console.log('>>> Age of first user is: ' + userAge + ' <<<');
+})();
