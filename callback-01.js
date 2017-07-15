9c6a98efecd19a46e1a714ea6eaf40e2e7c20190
Stefan Benz <stbenz88@gmail.com>
2017-07-15 20:35:52 +0200

    simplify more

--- callback-01.js
+++ callback-01.js
@@ -1,11 +1,11 @@
 const request = require('request');
 
-request.get({ url: 'http://localhost:3000/users', json: true }, (error, response) => {
+request.get({url: 'http://localhost:3000/users', json: true}, (error, response) => {
     const [firstUser] = response.body;
    
-    request.get({url: 'http://localhost:3000/users/' + firstUser + '/age'}, (error, response) => {
+    request.get({url: 'http://localhost:3000/users/' + firstUser + '/age', json: true}, (error, response) => {
 
-        const {userAge} = JSON.parse(response.body);
+        const {userAge} = response.body;
         console.log('>>> Age of first user is: ' + userAge + ' <<<');
     });
 });
