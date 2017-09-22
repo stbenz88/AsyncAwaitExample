f92317bbe187f40d558cd7a56bb43e32e9614dbb
Stefan Benz <stefan.benz@allianz.de>
2017-09-22 09:23:07 +0200

    add some comments to server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
+// Map datastructure -https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Map
 const userAges = new Map();
-
 userAges.set('Daniela', 50)
     .set('Stefan', 29)
     .set('Andrea', 26)
@@ -13,12 +13,14 @@ userAges.set('Daniela', 50)
 const app = express();
 
 app.get('/users', (request, response) => {
+    //New Map datastructure - https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Array/from
     const users = Array.from(userAges.keys());
 
     response.send(users);
 })
 
 app.get('/users/:user/age', (request, response) => {
+    //Destructuring assignment - http://es6-features.org/#ObjectMatchingShorthandNotation
     const {user} = request.params;
     const userAge = userAges.get(user);
 
